2d3d81e0a9173782156280dd45b113cd604fc714
Nataliia Strokan <nataliia.strokan@gmail.com>
2026-02-10 16:17:38 +0200

    Add input_error decorator for assistant bot

--- task4.py
+++ task4.py
@@ -1,3 +1,20 @@
+from functools import wraps
+
+
+def input_error(func):
+    @wraps(func)
+    def inner(*args, **kwargs):
+        try:
+            return func(*args, **kwargs)
+        except ValueError:
+            return "Give me name and phone please."
+        except IndexError:
+            return "Enter the argument for the command."
+        except KeyError:
+            return "Contact not found."
+    return inner
+
+
 def parse_input(user_input: str) -> tuple[str, list[str]]:
     parts = user_input.strip().split()
     if not parts:
@@ -5,31 +22,28 @@ def parse_input(user_input: str) -> tuple[str, list[str]]:
     return parts[0].lower(), parts[1:]
 
 
+@input_error
 def add_contact(args: list[str], contacts: dict[str, str]) -> str:
-    if len(args) != 2:
-        return "Usage: add <name> <phone>"
-    name, phone = args
+    name, phone = args          # potential ValueError / IndexError
     contacts[name.lower()] = phone
     return "Contact added."
 
 
+@input_error
 def change_contact(args: list[str], contacts: dict[str, str]) -> str:
-    if len(args) != 2:
-        return "Usage: change <name> <phone>"
     name, phone = args
     name = name.lower()
     if name not in contacts:
-        return "Contact not found."
+        raise KeyError
     contacts[name] = phone
     return "Contact updated."
 
 
+@input_error
 def show_phone(args: list[str], contacts: dict[str, str]) -> str:
-    if len(args) != 1:
-        return "Usage: phone <name>"
     name = args[0].lower()
     if name not in contacts:
-        return "Contact not found."
+        raise KeyError
     return contacts[name]
 
 
