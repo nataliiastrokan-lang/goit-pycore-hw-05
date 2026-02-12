7fb054472435cfc7d2e23d9b20557a57cd721a25
Nataliia Strokan <nataliia.strokan@gmail.com>
2026-02-12 09:12:09 +0200

    sum_profit usage

--- task2_profit_calculation.py
+++ task2_profit_calculation.py
@@ -8,7 +8,23 @@ def generator_numbers(text: str) -> Generator[float, None, None]:
     Numbers are assumed to be separated by spaces.
     """
     pattern = r"\b\d+\.\d+\b"
-
     for match in re.findall(pattern, text):
         yield float(match)
- 
+
+
+def sum_profit(text: str, func: Callable[[str], Generator[float, None, None]]) -> float:
+    """
+    Calculates the total sum of numbers produced by the generator function.
+    """
+    return sum(func(text))
+
+
+if __name__ == "__main__":
+    text = (
+        "Загальний дохід працівника складається з декількох частин: "
+        "1000.01 як основний дохід, доповнений додатковими надходженнями "
+        "27.45 і 324.00 доларів."
+    )
+
+    total_income = sum_profit(text, generator_numbers)
+    print(f"Загальний дохід: {total_income}")
